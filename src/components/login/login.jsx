ec61c2f9ab698f2396c74d9208061ca1452e9011
ksjung <ksjung@ksjungui-MacBookPro.local>
2021-01-17 11:25:20 +0900

    first

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -21,7 +21,7 @@ const Login = ({ authService }) => {
 
   useEffect(() => {
     authService.onAuthChange(user => {
-      user && goToMaker(user.id);
+      user && goToMaker(user.uid);
     });
   });
 
